aed5f9bd3f94125a5e4188f004e51059ed4564ce
Maheen Unzeelah <maheenunzeelah@gmail.com>
2022-08-24 19:13:49 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup';
+const schema=yup.object().shape({
+firstName:yup.string().required("First Name is required"),
+lastName:yup.string().required("Last Name is required"),
+email:yup.string().email().required() ,
 
+age: yup.number().typeError("Enter number only").required(),
+showDetails:yup.boolean(),
+details:yup.string().when('showDetails',{is:true,
+then:yup.string().required("Details are required")
+})
+})
 function App() {
+  const [formState,setFormState]=useState({
+    firstName:"maheen",
+    lastName:"unzeelah",
+    email:"abc@gmail.com",
+    age:23,
+  })
+  const [number,setNumber]=useState();
+  const {register,watch,handleSubmit,setValue, formState:{ errors,isDirty,isValid }}=useForm({  
+    mode: 'onChange',
+    resolver:yupResolver(schema),
+    // defaultValues:formState
+    
+  });
+  const watchShowDetails = watch("showDetails", false);
+  const submitForm=(data)=>{console.log(data)};
+  const allowOnlyNumber=(value)=>{
+
+    if(/^[0-9]*$/.test(value) )
+         setValue('age',value,{shouldValidate:true})
+    return value.replace(/[^0-9]/g, '')
+      
+    
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React Hooks Form Practice</h1>
+     <form onSubmit={handleSubmit(submitForm)}>
+          <input
+            type="text"
+            name="firstName"
+            {...register("firstName")} 
+            placeholder="First Name..."
+          />
+          <p>{errors.firstName?.message}</p>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name..."
+            {...register("lastName")} 
+        
+          />
+          <p> {errors.lastName?.message} </p>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email..."
+            {...register("email")} 
+       
+          />
+          <p> {errors.email?.message}</p>
+        <input
+
+           name="age"
+           placeholder="Age..."  
+           {...register("age", {
+            onChange:(e)=>{
+              const value=e.target.value.replace(/[^0-9]/g,'')
+              console.log(value)
+              setValue('age',value,{shouldValidate:true})
+            }
+           })}
+           
+           />
+          <p>{errors.age?.message} </p>
+          <div>
+          <label htmlFor="show-details">
+            <input
+              {...register('showDetails')}
+              type="checkbox"
+              name="showDetails"
+              value={true}
+              id="show-details"
+            />{' '}
+            Show Details
+          </label>
+        </div>
+      
+          {watchShowDetails &&<><input
+            type="text"
+            name="details"
+            placeholder="Enter Details..."
+            {...register("details")} 
+        
+          />
+          <p>{errors.details?.message}</p></>}
+             
+          <button type="submit" id="submit" disabled={!isValid} >Submit</button>
+        </form>
     </div>
   );
 }
